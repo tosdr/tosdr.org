88041d9261715fc12b1da2c3168ca5a9d587a6c2
Michiel de Jong <michiel@unhosted.org>
2013-01-28 17:31:57 +0700

    add code comments to buildIndexes.js

--- buildIndexes.js
+++ buildIndexes.js
@@ -1,3 +1,8 @@
+// this nodejs script reads all the data points from the points/ directory,
+// collects some data about which services and topics are mentioned in all
+// those data points, and writes that information out to two files in the
+// index/ directory
+
 var fs = require('fs');
 var service = {}, topic = {}, pending = 0;
 function writeOut() {
@@ -23,12 +28,25 @@ function addToServices(services, point) {
   //console.log(services);
   for(var i=0; i<services.length; i++) {
     if(!service[services[i]]) {
+      //points will be a list of data points about this service.
+      //links will be a list of links to the various terms and
+      //policy documents published by this service:
       service[services[i]] = {points: [], links: {}};
     }
     service[services[i]].points.push(point);
   }
 }
 function addToTopics(topics, point) {
+  //each data point has a topic. by creating a cross-index
+  //per topic, we can easily audit comparable data points
+  //about various services. for instance, if two websites both
+  //reserve the right to change their ToS without prior notice,
+  //and the reviewer of one site is much more condemning of this
+  //practice than the person reviewing the other site, then by
+  //checking a cross-view of all data points with the topic 'changes'
+  //can point out such a discrepancy, making it easier for us to
+  //make sure each data point is rated by a fair measure.
+
   //console.log('adding point "'+point+'" to topics:');
   //console.log(topics);
   for(var i=0; i<topics.length; i++) {
@@ -39,6 +57,9 @@ function addToTopics(topics, point) {
   }
 }
 function parsePointFile(id) {
+  //have a look at the files in the points/ directory of this
+  //repo to get a better feeling for what this function does
+
   var data = fs.readFileSync('points/'+id+'.json');
   var obj = JSON.parse(data.toString());
   if(obj.disputed || obj.irrelevant || obj.additional) {
@@ -63,7 +84,11 @@ function parsePointFile(id) {
   }
 }
 function parseServiceFile(id) {
+  //have a look at the files in the services/ directory of this
+  //repo to get a better feeling for what this function does
+
   console.log('SERVICE '+id);
+  //this is a bit of an ugly way to deal with asyncronicity, but it works:
   pending++;
   fs.readFile('services/'+id+'.json', function(err, data) {
     console.log(id);
@@ -79,11 +104,15 @@ function parseServiceFile(id) {
       }
     }
     pending--;
+    // "last person to leave switch off the lights please",
+    // or in this case, save everything to disk:
     if(pending==0) {
       writeOut();
     }
    });
 }
+//read all the points, and trigger the service files to be read
+//whenever mentioned.
 fs.readdir('points/', function(err, files) {
   if(err) {
     console.log(err);
@@ -100,3 +129,15 @@ fs.readdir('points/', function(err, files) {
     }
   }
 });
+//after this, we still need to call writeOut() to write all results to disk,
+//but the trouble is that since this code is all asynchronous, we don't know
+//when it is safe to call writeOut yet, or whether some data will still be arriving.
+//we deal with this using the 'pending' variable.
+
+//So the above code launches a number of parallel asynchronous execution paths for
+//parsing the points files. Each of these continues to launch a number of execution
+//paths for parsing the service files for all service files that were mentioned.
+//each of those increases the 'pending' variable, indicating that some processes
+//are still pending to complete.
+//when the last pending code path completes, 'pending' will be zero, and that callback
+//then takes up the responsibility of calling the writeOut() function.
