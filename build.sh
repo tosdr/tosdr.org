26c9d1125e3a8a2ab3444e3e8af1a1c76fb31d85
Michiel de Jong <michiel@unhosted.org>
2013-05-04 22:27:44 +0200

    improve build.sh and mention it in the README

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 node buildIndexes.js
 node render.js
 cd import
+mkdir -p points
+mkdir -p topics
+mkdir -p services
+mkdir -p index
 node prettifier.js | sh
 mv points/* ../points/
 mv topics/* ../topics/
 mv services/* ../services/
 mv index/* ../index/
 cd ..
+git status
