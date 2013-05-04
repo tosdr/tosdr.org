622b3537b87ae6c2d8a358231cd0112e991146ca
Michiel de Jong <michiel@unhosted.org>
2013-05-04 20:10:22 +0200

    prettify index, topics, and services

--- import/prettifier.js
+++ import/prettifier.js
@@ -2,7 +2,28 @@ var fs = require('fs');
 fs.readdir('../points/', function(err, files) {
   for(var i=0; i<files.length; i++) {
     if(files[i]!='README.md') {
-      console.log('echo "starting '+files[i]+'"; cat ../points/'+files[i]+' | python -mjson.tool > prettified/'+files[i]+'; echo "finishing '+files[i]+'"');
+      console.log('echo "starting '+files[i]+'"; cat ../points/'+files[i]+' | python -mjson.tool > points/'+files[i]+'; echo "finishing '+files[i]+'"');
+    }
+  }
+});
+fs.readdir('../topics/', function(err, files) {
+  for(var i=0; i<files.length; i++) {
+    if(files[i]!='README.md') {
+      console.log('echo "starting '+files[i]+'"; cat ../topics/'+files[i]+' | python -mjson.tool > topics/'+files[i]+'; echo "finishing '+files[i]+'"');
+    }
+  }
+});
+fs.readdir('../services/', function(err, files) {
+  for(var i=0; i<files.length; i++) {
+    if(files[i]!='README.md') {
+      console.log('echo "starting '+files[i]+'"; cat ../services/'+files[i]+' | python -mjson.tool > services/'+files[i]+'; echo "finishing '+files[i]+'"');
+    }
+  }
+});
+fs.readdir('../index/', function(err, files) {
+  for(var i=0; i<files.length; i++) {
+    if(files[i]!='README.md') {
+      console.log('echo "starting '+files[i]+'"; cat ../index/'+files[i]+' | python -mjson.tool > index/'+files[i]+'; echo "finishing '+files[i]+'"');
     }
   }
 });
