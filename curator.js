ba08ab11d488534fba32aa793f2caa9da24f8de8
Michiel de Jong <michiel@unhosted.org>
2013-05-05 21:25:13 +0200

    process post

--- curator.js
+++ curator.js
@@ -15,35 +15,79 @@ function savePoint(filename) {
 }
 
 function displayPoint(res, filename, reason, data) {
-  res.write('<li>'+filename+' '+reason+JSON.stringify(data)+'</li>');
+  res.write('<li><a href="?'+filename+'">'+filename+'</a> '+reason+JSON.stringify(data)+'</li>');
   console.log(filename);
 }
 
-function displayPoints(res) {
-	var before;
-  files = fs.readdirSync('points/');
-	for(var i=0; i<files.length; i++) {
-		if(files[i]!='README.md') {
-			addFile(files[i]);
-			if(!points[files[i]].id) {
-				displayPoint(res, files[i], 'no id', points[files[i]]);
-			}
-			if(!points[files[i]].title) {
-				displayPoint(res, files[i], 'no title', points[files[i]]);
-			}
-			if(!points[files[i]].irrelevant && !points[files[i]].service) {
-				displayPoint(res, files[i], 'no service', points[files[i]]);
-			}
-		}
-	}
-	//console.log(points);
+function displayField(res, point, field) {
+  if(typeof(point[field])=='string') {
+    point[field]=point[field].split('"').join('&quot;');
+  }
+  res.write(field+': <input value="'+point[field]+'" name="'+field+'"/> <br>');
+}
+function displayForm(res, filename) {
+  var point = points[filename];
+  res.write('<form method="POST">');
+  displayField(res, point, 'id');
+  displayField(res, point, 'title');
+  displayField(res, point, 'service');
+  displayField(res, point, 'irrelevant');
+  displayField(res, {filename: filename}, 'filename');
+  res.write('<input type="submit"></form>');
+  res.write('JSON: <textarea>'+fs.readFileSync('points/'+filename)+'</textarea>');
 }
 
+function displayPoints(res) {
+  for(var i in points) {
+    if(!points[i].id) {
+      displayPoint(res, i, 'no id', points[i]);
+    }
+    if(!points[i].title) {
+      displayPoint(res, i, 'no title', points[i]);
+    }
+    if(!points[i].irrelevant && !points[i].service) {
+      displayPoint(res, i, 'no service', points[i]);
+    }
+  }
+	//console.log(points);
+}
+function processPost(req) {
+  var str='';
+  req.on('data', function(chunk) {
+    str += chunk;
+  });
+  req.on('end', function() {
+    var pairs = str.split('&');
+    var incoming = {};
+    for(var i=0; i<pairs.length; i++) {
+      var parts = pairs[i].split('=');
+      incoming[parts[0]]=parts[1];
+    }
+    for(var i in incoming) {
+      if(i!='filename') {
+        points[incoming.filename][i]=incoming[i];
+      }
+    }
+    savePoint(incoming.filename);
+  });
+}
 //...
+files = fs.readdirSync('points/');
+for(var i=0; i<files.length; i++) {
+	if(files[i]!='README.md') {
+		addFile(files[i]);
+  }
+}
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {});
   res.write('<html><ul>');
-  displayPoints(res);
+  var point = req.url.substring(2);
+  if(point.length) {
+    processPost(req);
+    displayForm(res, point);
+  } else {
+    displayPoints(res);
+  }
   res.end('</ul></html>');
 });
 server.listen(21337);
