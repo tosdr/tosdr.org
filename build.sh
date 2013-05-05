009cf8194104e41f3a17c729f7a5a9e1d066d0b1
Hugo Roy <hugo@fsfe.org>
2013-05-05 15:06:02 +0200

    typos in class a

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-node buildIndexes.js
-node render.js
+nodejs buildIndexes.js
+nodejs render.js
 cd import
 mkdir -p points
 mkdir -p topics
 mkdir -p services
 mkdir -p index
-node prettifier.js | sh
+nodejs prettifier.js | sh
 mv points/* ../points/
 mv topics/* ../topics/
 mv services/* ../services/
