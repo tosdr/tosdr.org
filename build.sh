26c3bec9270106f90a0f91a503706ce3e55095df
Michiel de Jong <michiel@unhosted.org>
2013-05-08 10:04:26 -0500

    build no the crawl server

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-nodejs buildIndexes.js
-nodejs render.js
+node buildIndexes.js
+node render.js
 cd import
 mkdir -p points
 mkdir -p topics
 mkdir -p services
 mkdir -p index
-nodejs prettifier.js | sh
+node prettifier.js | sh
 mv points/* ../points/
 mv topics/* ../topics/
 mv services/* ../services/
