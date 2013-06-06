84814e2083bf56e91602d0268614e9843f38c479
Michiel de Jong <michiel@unhosted.org>
2013-06-06 05:21:48 -0500

    don't split on commas

--- buildIndexes.js
+++ buildIndexes.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 var service = {}, topic = {}, pending = 0;
 function writeOut() {
   //console.log(service);
-  fs.writeFile('index/services.json', JSON.stringify(service).split(',').join(',\n'), function(err) {
+  fs.writeFile('index/services.json', JSON.stringify(service), function(err) {
     if(err) {
       console.log('error writing index/services.json');
     } else {
@@ -15,7 +15,7 @@ function writeOut() {
     }
   });
   console.log(topic);
-  fs.writeFile('index/topics.json', JSON.stringify(topic).split(',').join(',\n'), function(err) {
+  fs.writeFile('index/topics.json', JSON.stringify(topic), function(err) {
     if(err) {
       console.log('error writing index/topics.json');
     } else {
