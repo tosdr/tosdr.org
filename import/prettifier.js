6da587794b2007df7da63802395c1f5afcfd7631
ggrin <ggrin@lavabit.com>
2013-07-15 19:36:05 +0200

    pretifier.js became pure nodejs

--- import/prettifier.js
+++ import/prettifier.js
@@ -1,29 +1,21 @@
 var fs = require('fs');
-fs.readdir('../points/', function(err, files) {
-  for(var i=0; i<files.length; i++) {
-    if(files[i]!='README.md') {
-      console.log('echo "starting '+files[i]+'"; cat ../points/'+files[i]+' | python -mjson.tool > points/'+files[i]+'; echo "finishing '+files[i]+'"');
+
+function process(path){
+  fs.readdir('../'+path, function(err, files){
+    for(var i=0; i<files.length; i++) {
+      if( files[i].match(/\.json$/) ) {
+        try {
+          console.log('starting '+path+'/'+files[i]); 
+          var item = JSON.parse(fs.readFileSync('../'+path+'/'+files[i]));
+          fs.writeFileSync(path+'/'+files[i], JSON.stringify(item,undefined, 2));
+        } catch(e) {
+          console.error("SOMETHINGS WRONG",e)
+        }
+        
+      }
     }
-  }
-});
-fs.readdir('../topics/', function(err, files) {
-  for(var i=0; i<files.length; i++) {
-    if(files[i]!='README.md') {
-      console.log('echo "starting '+files[i]+'"; cat ../topics/'+files[i]+' | python -mjson.tool > topics/'+files[i]+'; echo "finishing '+files[i]+'"');
-    }
-  }
-});
-fs.readdir('../services/', function(err, files) {
-  for(var i=0; i<files.length; i++) {
-    if(files[i]!='README.md') {
-      console.log('echo "starting '+files[i]+'"; cat ../services/'+files[i]+' | python -mjson.tool > services/'+files[i]+'; echo "finishing '+files[i]+'"');
-    }
-  }
-});
-fs.readdir('../index/', function(err, files) {
-  for(var i=0; i<files.length; i++) {
-    if(files[i]!='README.md') {
-      console.log('echo "starting '+files[i]+'"; cat ../index/'+files[i]+' | python -mjson.tool > index/'+files[i]+'; echo "finishing '+files[i]+'"');
-    }
-  }
-});
+  })
+}
+
+
+['points', 'topics', 'services', 'index'].forEach(process);
