b356204e24dafb5f65bb1f9a0b4c56d4aeb42075
ggrin <ggrin@lavabit.com>
2013-07-23 00:00:54 +0200

    alphabetical oder of json data files

--- import/prettifier.js
+++ import/prettifier.js
@@ -1,5 +1,14 @@
 var fs = require('fs');
 
+function stringify_sorted(obj)
+{
+	var tObj = {};
+	Object.keys(obj).sort().forEach( function(key) {
+		tObj[key] = obj[key]
+	} )
+    return JSON.stringify(tObj);
+}
+
 function process(path){
   fs.readdir('../'+path, function(err, files){
     for(var i=0; i<files.length; i++) {
@@ -7,7 +16,7 @@ function process(path){
         try {
           console.log('starting '+path+'/'+files[i]); 
           var item = JSON.parse(fs.readFileSync('../'+path+'/'+files[i]));
-          fs.writeFileSync(path+'/'+files[i], JSON.stringify(item,undefined, 2));
+          fs.writeFileSync(path+'/'+files[i], stringify_sorted(item,undefined, 2));
         } catch(e) {
           console.error("SOMETHINGS WRONG",e)
         }
