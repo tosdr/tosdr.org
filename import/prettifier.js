25b1d367b53c2e3d9445cdeef46f4a6c1db868f6
ggrin <ggrin@lavabit.com>
2013-07-29 18:14:50 +0200

    done some prettifier improvements

--- import/prettifier.js
+++ import/prettifier.js
@@ -1,26 +1,31 @@
 var fs = require('fs');
 
-function stringify_sorted(obj)
+function sortObject(obj)
 {
-	var tObj = {};
-	Object.keys(obj).sort().forEach( function(key) {
-		tObj[key] = obj[key]
-	} )
-    return JSON.stringify(tObj);
+  if(typeof obj !== 'object')
+    return obj;
+  var tObj = {};
+  Object.keys(obj).sort().forEach( function(key) {
+    tObj[key] = sortObject(obj[key])
+  } )
+  return tObj;
 }
 
 function process(path){
   fs.readdir('../'+path, function(err, files){
+    if(err){
+      console.log("Directory ../"+path+" caused an Error", err);
+      return
+    }
     for(var i=0; i<files.length; i++) {
       if( files[i].match(/\.json$/) ) {
         try {
           console.log('starting '+path+'/'+files[i]); 
           var item = JSON.parse(fs.readFileSync('../'+path+'/'+files[i]));
-          fs.writeFileSync(path+'/'+files[i], stringify_sorted(item,undefined, 2));
+          fs.writeFileSync(path+'/'+files[i], JSON.stringify(sortObject(item),undefined, 2));
         } catch(e) {
-          console.error("SOMETHINGS WRONG",e)
-        }
-        
+          console.error("SOMETHING IS WRONG with ",path+files[i],e)
+        }        
       }
     }
   })
