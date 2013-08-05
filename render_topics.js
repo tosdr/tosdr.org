15ffe826ec464e6265c1040fc2c5238c638b9739
ggrin <ggrin@lavabit.com>
2013-08-06 00:48:42 +0200

    mustache powered templates, works fine checkout index-test.html and topics-test.html

--- render_topics.js
+++ render_topics.js
@@ -13,7 +13,39 @@ db.templates['topics.html']({topics:topics}));
 var services = []
 for(var t in db.services){
   var service = db.services[t];
+  if(service.points) {
+    service.points = service.points.filter(function(a){return a}).sort(function(a,b){
+      return Math.abs(b.score)-Math.abs(a.score);
+    })
+    if(service.links){
+      var links = [];
+      for(var k in service.links){
+        var link = service.links[k];
+        if(!link.name)
+          link.name = k;
+        links.push(link);
+      }
+      service.links = links
+    }
+    //only showng the 5 most important points in the index.html
+    service.renderables = service.points.slice(0,5);
+  }
+  //console.log(service.name, service.renderables);
   services.push(service);
 }
+services = services.sort(function(a,b){
+  if(!a.alexa)
+    a.alexa = 1000000;
+  if(!b.alexa)
+    b.alexa = 1000000;
+  return a.alexa - b.alexa;
+});
 fs.writeFileSync('./index-test.html', 
                  db.templates['index.html']({services:services}) )
+
+var popups = {};
+services.forEach(function(s){
+  popups[s.id] = db.templates['services.js'](s)
+})
+
+fs.writeFileSync('./services-test.js', "var popupsContent = "+JSON.stringify(popups));
