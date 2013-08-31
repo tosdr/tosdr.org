4e964fb20c2aec4e5ba8d346b3a20cf1e8ee3010
ggrin <ggrin@lavabit.com>
2013-08-31 16:00:31 +0200

    don't realy know why those changed

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 node buildIndexes.js
-node render.js
+#node render.js
 lessc -x css/custom.less css/custom.css
 cd import
-mkdir -p points topics services index
+mkdir -p points topics services index cases
 node prettifier.js
 mv points/* ../points/
 mv topics/* ../topics/
 mv services/* ../services/
+mv cases/* ../cases
 mv index/* ../index/
+
 cd ..
 git status
