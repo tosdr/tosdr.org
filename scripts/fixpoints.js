567b3b73fe0593c443f669c60e22a75b71fe3fa8
Michiel de Jong <michiel@tosdr.org>
2013-09-30 16:21:29 +0300

    move irrelevant into tosdr

--- scripts/fixpoints.js
+++ scripts/fixpoints.js
@@ -3,7 +3,7 @@ var fs = require('fs'),
 fs.readdir('points/', function(err, files) {
   if(err) {
     console.log(err, 'readdir');
-    exit(1);
+    process.exit(1);
   } else {
     for(var i=0; i<files.length; i++) {
       if(files[i] != 'README.md') {
@@ -14,47 +14,60 @@ fs.readdir('points/', function(err, files) {
 });
 function doFile(fileName) {
   fs.readFile('points/'+fileName, function(err, data) {
-    if(err) {
-      console.log(err, fileName);
-      exit(1);
-    } else {
-      var obj, changed = false;
-      try {
-        obj = JSON.parse(data);
-      } catch(e) {
-        console.log(e, fileName);
-        exit(1);
-      }
-      if(obj.disputed) {
-        obj.tosdr.disputed = true;
-        changed = true;
-      }
-      if(!obj.services && obj.service) {
-        if(Array.isArray(obj.service)) {
-          obj.services = obj.service;
-        } else {
-          obj.services = [obj.service];
+    try {
+      if(err) {
+        console.log(err, fileName);
+        process.exit(1);
+      } else {
+        var obj, changed = false;
+        try {
+          obj = JSON.parse(data);
+        } catch(e) {
+          console.log(e, fileName);
+          process.exit(1);
         }
-        changed = true;
-      }
-      if(!obj.topics && obj.topic) {
-        if(Array.isArray(obj.topic)) {
-          obj.topics = obj.topic;
-        } else {
-          obj.topics = [obj.topic];
+        if(typeof(obj.tosdr)!='object' || Array.isArray(obj.tosdr)) {
+          obj.tosdr = {};
         }
-        changed = true;
-      }
-      if(changed) {
-        fs.writeFile('points/'+fileName, prettyjson(obj), function(err) {
-          if(err) {
-            console.log(e, filename);
-            exit(1);
+        if(obj.disputed) {
+          obj.tosdr.disputed = true;
+          changed = true;
+        }
+        if(obj.irrelevant) {
+          obj.tosdr.irrelevant = true;
+          obj.tosdr.reason = obj.irrelevant;
+          changed = true;
+        }
+        if(!obj.services && obj.service) {
+          if(Array.isArray(obj.service)) {
+            obj.services = obj.service;
           } else {
-            console.log('fixed '+fileName);
+            obj.services = [obj.service];
           }
-        });
+          changed = true;
+        }
+        if(!obj.topics && obj.topic) {
+          if(Array.isArray(obj.topic)) {
+            obj.topics = obj.topic;
+          } else {
+            obj.topics = [obj.topic];
+          }
+          changed = true;
+        }
+        if(changed) {
+          fs.writeFile('points/'+fileName, prettyjson(obj), function(err) {
+            if(err) {
+              console.log(e, filename);
+              process.exit(1);
+            } else {
+              console.log('fixed '+fileName);
+            }
+          });
+        }
       }
-    }
+    } catch(e) {
+      console.log(e, fileName);
+      process.exit(1);
+    } 
   });
 }
