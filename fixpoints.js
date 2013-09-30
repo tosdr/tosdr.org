678dbfc1e64a6a9774be0995b2ea7e13cea04154
Michiel de Jong <michiel@tosdr.org>
2013-09-30 10:12:43 +0300

    make sure 'services' field exists - ref https://github.com/tosdr/tosdr.org/wiki/Specification:-points

---
+++ fixpoints.js
@@ -0,0 +1,45 @@
+var fs = require('fs'),
+  prettyjson = require('./prettyjson');
+fs.readdir('points/', function(err, files) {
+  if(err) {
+    console.log(err, 'readdir');
+    exit(1);
+  } else {
+    for(var i=0; i<files.length; i++) {
+      if(files[i] != 'README.md') {
+        doFile(files[i]);
+      }
+    }
+  }
+});
+function doFile(fileName) {
+  fs.readFile('points/'+fileName, function(err, data) {
+    if(err) {
+      console.log(err, fileName);
+      exit(1);
+    } else {
+      var obj;
+      try {
+        obj = JSON.parse(data);
+      } catch(e) {
+        console.log(e, fileName);
+        exit(1);
+      }
+      if(!obj.services && obj.service) {
+        if(Array.isArray(obj.service)) {
+          obj.services = obj.service;
+        } else {
+          obj.services = [obj.service];
+        }
+        fs.writeFile('points/'+fileName, prettyjson(obj), function(err) {
+          if(err) {
+            console.log(e, filename);
+            exit(1);
+          } else {
+            console.log('fixed '+fileName);
+          }
+        });
+      }
+    }
+  });
+}
