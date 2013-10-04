c1eff4851b4c52089e6beb588677180a969ebb14
Michiel de Jong <michiel@tosdr.org>
2013-10-04 08:44:15 +0300

    make checkcases script more generic

---
+++ scripts/cases.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'changes-notice': {
+     1: { name: 'user needs to check tosback.org', point: 'bad', score: 70 },
+     2: { name: 'user is notified on the day', point: 'neutral', score: 0 },
+     3: { name: 'user is notified a week or more in advance', point: 'good', score: 30 },
+     4: { name: 'user is notified a month or more in advance', point: 'good', score: 40 }
+  }
+};
