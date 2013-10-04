ac5989d5012ca4408e820bb7fd4bee131da2814f
Michiel de Jong <michiel@tosdr.org>
2013-10-04 09:41:42 +0300

    add cases for cookies topic and added Google and Facebook to Amazon point

--- scripts/cases.js
+++ scripts/cases.js
@@ -9,4 +9,11 @@ module.exports = {
      3: { name: 'user is notified a week or more in advance', point: 'good', score: 30 },
      4: { name: 'user is notified a month or more in advance', point: 'good', score: 40 },
   },
+  'cookies': {
+     1: { name: 'only temporary session cookies', point: 'good', score: 20 },
+     2: { name: 'requires first-party cookies', point: 'neutral', score: 10 },
+     3: { name: 'third-party cookies, but with opt-out instructions', point: 'bad', score: 5 },
+     3: { name: 'sets third-party cookies and/or ads', point: 'bad', score: 10 },
+     4: { name: 'tracks you on other websites', point: 'bad', score: 60 },
+  },
 };
