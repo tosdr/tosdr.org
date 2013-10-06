bef35750736334f70fc40fc31850d6a88c1f91df
Michiel de Jong <michiel@unhosted.org>
2013-10-06 15:54:44 +0300

    *temporarily* only display point under its first topic

--- scripts/buildIndexes.js
+++ scripts/buildIndexes.js
@@ -57,6 +57,7 @@ function addToTopics(topics, point) {
     if(topic[topics[i]].indexOf(point) == -1) {
       topic[topics[i]].push(point);
     }
+    break;
   }
 }
 function parsePointFile(id) {
