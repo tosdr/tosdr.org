a47711f22b18d0714e8ad0f0b0880518812f14f2
Michiel de Jong <michiel@unhosted.org>
2014-09-26 19:43:35 +0200

    add api/1/service

--- scripts/generateApiFiles.js
+++ scripts/generateApiFiles.js
@@ -7,5 +7,5 @@ for (i in services) {
   for (j = 0; j < obj.points.length; j++) {
     obj.pointsData[obj.points[j]] = JSON.parse(fs.readFileSync('points/'+obj.points[j]+'.json'));
   }
-  fs.writeFileSync('api/1/digest/'+i+'.json', JSON.stringify(obj));
+  fs.writeFileSync('api/1/service/'+i+'.json', JSON.stringify(obj));
 }
