367c592672593ace44c9e2008dd86662f84e8e63
Hugo Roy <hugo@fsfe.org>
2014-09-27 12:07:11 +0200

    help block before submitting

--- js/submit-point.js
+++ js/submit-point.js
@@ -94,9 +94,9 @@ $(document).ready(function(){
 
                             '</fieldset>' +
                             '<div class="form-actions">' +
+                                '<p class="help-block">Submitting as ' + email + ' <a href="" class="signoutButton">(Not your email address? Log out!)</a></p>' +
                                 '<button class="btn btn-primary" id="postPoint">Send</button>' +
                                 
-                    '<p class="help-block">Submitting as ' + email + ' <a href="" class="signoutButton">(Not your email address? Log out!)</a></p>' +
                             '</div>' +
                         '</form>');
     showServiceFields();
